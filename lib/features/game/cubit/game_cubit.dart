244c3847bd57379ca1d337071a5f8e4436294ae5
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-06-02 16:09:49 +0300

    refactor cubit classes

--- lib/features/game/cubit/game_cubit.dart
+++ lib/features/game/cubit/game_cubit.dart
@@ -8,7 +8,17 @@ class GameCubit extends Cubit<GameState> {
   GameCubit(
     this._router,
   ) : super(
-          const GameState.initial(hiddenWord: 'КОБЗА'),
+          GameState(
+            hiddenWord: 'КОБЗА',
+            answers: defaultAnswers,
+            keyboard: createKeyboard(
+              [
+                ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ґ', 'Ш', 'Щ', 'З', 'Х'],
+                ['Ф', 'І', 'Ї', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Є'],
+                ['<', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', '>'],
+              ],
+            ),
+          ),
         );
 
   final AppRouter _router;
--- lib/features/game/cubit/game_state.dart
+++ lib/features/game/cubit/game_state.dart
@@ -1,16 +1,41 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kobza/core/model/one_letter.dart';
 import 'package:kobza/core/model/one_word.dart';
 
 part 'game_state.freezed.dart';
 
+typedef Keyboard = List<List<OneLetter>>;
+
+final defaultAnswers = List.generate(
+  6,
+  (a) => List.generate(
+    5,
+    (l) => const OneLetter(letter: '', letterState: 0),
+  ),
+);
+
+Keyboard createKeyboard(List<List<String>> alphabet) {
+  return alphabet
+      .map((line) =>
+          line.map((l) => OneLetter(letter: l, letterState: 0)).toList())
+      .toList();
+}
+
 @freezed
 class GameState with _$GameState {
-  const factory GameState.initial({
+  const factory GameState({
     required String hiddenWord,
-  }) = InitialGameState;
-  const factory GameState.matchCheck({
-    required String hiddenWord,
-    required OneWord currentWord,
-  }) = MatchCheckState;
-  const factory GameState.guessed() = GuessedGameState;
+    required List<List<OneLetter>> answers,
+    required Keyboard keyboard,
+  }) = _GameState;
+  // const factory GameState.matchCheck({
+  //   required String hiddenWord,
+  //   required OneWord currentWord,
+  // }) = MatchCheckState;
+  // const factory GameState.guessed() = GuessedGameState;
+}
+
+extension XGameState on GameState {
+  bool get guessed =>
+      answers.any((line) => line.every((l) => l.letterState == 2));
 }
