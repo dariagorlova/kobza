32de2ab28f545617efe773f10ff3b36c388040d0
dariagorlova <98218092+dariagorlova@users.noreply.github.com>
2022-06-08 09:52:03 +0300

    change test for marked letter

--- test/step/i_see_letter_marked_at_guessed.dart
+++ test/step/i_see_letter_marked_at_guessed.dart
@@ -1,18 +1,36 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kobza/core/model/one_letter.dart';
+import 'package:kobza/features/game/screen/widgets/letter_box.dart';
 
 Future<void> iSeeLetterMarkedAtGuessed(
   WidgetTester tester,
   String letter,
 ) async {
-  const colorGuessed = Colors.green;
+  // const colorGuessed = Colors.green;
+
+  // final childWidget = Text(letter);
+  // await tester.pumpWidget(Container(
+  //   child: childWidget,
+  // ),);
+  // expect(find.byWidget(childWidget), findsWidgets);
+
   expect(
     find.byWidgetPredicate(
       (widget) =>
-          widget is Container &&
-          widget.child == Text(letter) &&
-          widget.color == colorGuessed,
+          widget is LetterBox &&
+          widget.letter.letter == letter &&
+          widget.letter.letterState == LetterState.correctly,
     ),
     findsWidgets,
   );
+
+  // expect(
+  //   find.byWidgetPredicate(
+  //     (widget) =>
+  //         widget is Container &&
+  //         widget.child == Text(letter) &&
+  //         widget.color == colorGuessed,
+  //   ),
+  //   findsWidgets,
+  // );
 }
